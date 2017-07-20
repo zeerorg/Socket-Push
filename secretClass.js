aa0002e757c7af573fdea1d394fd06aaa6faa9e1
Rishabh (zeerorg) <rishabh4325@gmail.com>
2017-07-20 17:24:43 +0530

    Refined repository

--- secretClass.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Secrets = (function () {
-    function Secrets() {
-        this.mongoPort = "27017";
-    }
-    return Secrets;
-}());
-exports.Secrets = Secrets;
